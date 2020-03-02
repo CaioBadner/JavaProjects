b9b9b80ce1f5e80ba4eeb429a9aa33f32e630bbd
CaioBadner <59920967+CaioBadner@users.noreply.github.com>
2020-03-02 04:44:49 +0200

    Update EmailChecker.java

--- EmailChecker.java
+++ EmailChecker.java
@@ -16,6 +16,8 @@ public class EmailChecker {
 	}
 
 	private static boolean isEmailValid(String email) {
+		
+		//this is a quick check to make sure it's even worth breaking down the email
 		if (email.length() < 3) {
 			return false;
 		}
@@ -26,13 +28,16 @@ public class EmailChecker {
 		int dotPos = 0;
 		String extraName = ""; 
 		
+		//now we will just break down the string we received into parts
 		for (int i = 0; i < email.length(); i++) {
 			char character = email.charAt(i);
 			if (character == '@') {
+				//we can also return false immediately if there is more than one strudel @
 				if (strudelPos > 0) {
 					return false;
 				}
 				strudelPos = i;
+				//we have to continue here to make sure this character doesn't get added to the other Strings
 				continue;
 			} else if (character == '.') {
 				dotPos = i;
@@ -48,8 +53,12 @@ public class EmailChecker {
 			}
 			
 		}
-		if (strudelPos > 0 && dotPos > strudelPos && (extraName.equals("com") || extraName.equals("gov") || extraName.equals("org") 
-													|| extraName.equals("coil") || extraName.equals("combr"))) {
+		//and here is the main check
+		//if there was one strudel and at least one point after the strudel, and if the email ends with one of these,
+		//then we say it's true. 
+		//future implementation will add a better extraName check that will be more inclusive
+		if (strudelPos > 0 && dotPos > strudelPos && (extraName.equals("com") || extraName.equals("gov") || 
+					 extraName.equals("org") || extraName.equals("coil") || extraName.equals("combr"))) {
 			return true;
 		}
 				

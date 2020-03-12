b3bf5576c9d8a3dec20b397f037a211404de223e
CaioBadner <59920967+CaioBadner@users.noreply.github.com>
2020-03-12 21:30:42 +0200

    Update Maze.java

--- PathFinder/src/Maze.java
+++ PathFinder/src/Maze.java
@@ -1,28 +1,68 @@
 
 public class Maze {
 
-	private static boolean [] [] maze;
+	private static String mazeName;
 	private static int length;
+	private static boolean [] [] maze;
 	
-	public Maze (int length) {
-		Maze.length = length;
-		Maze.maze = new boolean [length][length];
-		Maze.maze[0][0] = true;
-		Maze.maze[length-1][length-1] = true;
-	}
+	private final static int DEFAULT_LENGTH = 10;
+	private final static String DEFAULT_NAME = "NewMaze";
 	
 	public Maze () {
-		Maze.maze = maze01;
+		Maze.mazeName = DEFAULT_NAME;
+		Maze.length = DEFAULT_LENGTH;
+		Maze.maze = new boolean [DEFAULT_LENGTH][DEFAULT_LENGTH];
+		Maze.maze[0][0] = true;
+		Maze.maze[DEFAULT_LENGTH-1][DEFAULT_LENGTH-1] = true;
 	}
 	
-	public int getLength() {
+	public static void setFullMaze (String newMazeName, int newLength, boolean[][] newMaze) {
+		Maze.mazeName = newMazeName;
+		Maze.length = newLength;
+		Maze.maze = newMaze;
+	}
+	
+	public static void setFullMaze (String newMazeName,boolean[][] newMaze) {
+		Maze.mazeName = newMazeName;
+		Maze.length = newMaze.length;
+		Maze.maze = newMaze;
+	}
+	
+	public static String getMazeName() {
+		return mazeName;
+	}
+
+	public static void setMazeName(String mazeName) {
+		Maze.mazeName = mazeName;
+	}
+
+	public static int getLength() {
 		return length;
 	}
-
-	public static boolean [] [] getMaze() {
-		return maze;
+	
+	public static void setLength(int length) {
+		Maze.length = length;
 	}
 
+	public static boolean [][] getMaze() {
+		return maze;
+	}
+	
+	public static void setMaze(boolean [][] maze) {
+		Maze.maze = maze;
+	}
+	
+	public static boolean [][] getMazeCopy() {
+		boolean [][] mazeCopy = new boolean[length][length];
+		for (int x = 0; x < mazeCopy.length; x++) {
+			for (int y = 0; y < mazeCopy.length; y++) {
+				mazeCopy[x][y] = maze[x][y];
+			}
+		}
+		return mazeCopy;
+	}
+
+	//the classic maze that it still remembers for good old times
 	public static final boolean [] [] maze01 =     {{true,true,true,false,true,false,true,true,true,true},
 													{true,false,true,false,true,false,true,false,false,true},
 													{true,false,true,true,true,true,true,false,true,true},
@@ -33,14 +73,7 @@ public class Maze {
 													{true,false,true,true,true,true,false,true,true,true},
 													{true,false,false,false,false,true,false,true,false,false},
 													{true,true,true,true,true,true,false,true,true,true}};
-																
-									
-/*
-public static boolean [] [] maze = 	{{true,false,true,true,true},
-										{true,false,true,false,true},
-										{true,true,true,true,true},
-										{true,false,true,false,true},
-										{true,true,true,true,true}};
+	
 
-*/
+
 }
